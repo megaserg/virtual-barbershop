8daa99537196bb90d415de8f634a485d114752f6
vap.poljakov@gmail.com <vap.poljakov@gmail.com@93983804-0382-454f-7401-2d3674e563d1>
2012-04-14 18:51:34 +0000

    folders configuration added

--- trunk/VirtualBarbershop/web-ui/src/main/java/edu/virtual/barbershop/ActionFactory.java
+++ trunk/VirtualBarbershop/web-ui/src/main/java/edu/virtual/barbershop/ActionFactory.java
@@ -1,9 +1,15 @@
 package edu.virtual.barbershop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ActionFactory {
-    public Action create(String actionName) {
+    private static final Logger logger = LoggerFactory.getLogger(ActionFactory.class);
+
+    public Action create(String actionName, Storage storage) {
         if (actionName.equalsIgnoreCase("mergeImages")) {
             ImageAction ia = new ImageAction();
+            ia.setStorage(storage);
             return ia;
         } else {
             throw new RuntimeException(actionName + " cannot be found.");
--- trunk/VirtualBarbershop/web-ui/src/main/java/edu/virtual/barbershop/VirtualBarbershopServlet.java
+++ trunk/VirtualBarbershop/web-ui/src/main/java/edu/virtual/barbershop/VirtualBarbershopServlet.java
@@ -1,5 +1,10 @@
 package edu.virtual.barbershop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,11 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public final class VirtualBarbershopServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(VirtualBarbershopServlet.class);
 
+    private boolean started = false;
     private ActionFactory factory = new ActionFactory();
 
+
     public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Action action = factory.create(req.getParameter("action"));
+        WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+        Storage storage = (Storage)ctx.getBean("storage");
+        if (!started) {
+            storage.initBaseFolder(getServletContext().getRealPath("/"));
+            started = true;
+        }
+
+        Action action = factory.create(req.getParameter("action"), storage);
         action.perform(req, resp);
     }
 }
