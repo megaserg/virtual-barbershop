ecb2c8ba254e030c3f6d2c8d0d3f02ae89ab86e9
vap.poljakov@gmail.com <vap.poljakov@gmail.com@93983804-0382-454f-7401-2d3674e563d1>
2012-05-02 17:38:12 +0000

    default db info added

--- trunk/VirtualBarbershop/utils/src/main/java/edu/virtual/barbershop/Storage.java
+++ trunk/VirtualBarbershop/utils/src/main/java/edu/virtual/barbershop/Storage.java
@@ -73,6 +73,19 @@ public class Storage {
             return false;
         }
 
+        if (!createTables()) {
+            return true;
+        }
+
+        insertHaircutsInfo();
+        insertBarbershopsInfo();
+        insertOffersInfo();
+        insertCollageInfo();
+
+        return true;
+    }
+
+    private boolean createTables() {
         try {
             Statement statement = connection.createStatement();
 
@@ -102,11 +115,91 @@ public class Storage {
             statement.executeUpdate(offer);
 
             statement.close();
-        } catch (SQLException s) {
-            logger.info("DB already created.");
-        }
 
-        return true;
+            return true;
+        } catch (SQLException e) {
+            logger.info("DB already created.");
+            return false;
+        }
+    }
+
+    private void insertHaircutsInfo() {
+        try {
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht1', 'massive')");
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht2', 'progressive')");
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht3', 'mixed')");
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht4', 'massive')");
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht5', 'progressive')");
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht6', 'uniform')");
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht7', 'mixed')");
+            statement.executeUpdate("insert into Haircut (id, shape) values ('ht8', 'graded')");
+
+            statement.close();
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private void insertBarbershopsInfo() {
+        try {
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate("insert into Barbershop (name, address, phone) values " +
+                    "('Barbershop \"Svetlana\"', '325 Wall St. New York', '952-234-2351')");
+
+            statement.executeUpdate("insert into Barbershop (name, address, phone) values " +
+                    "('Barbershop \"Mimoza\"', '36 Round Sq. San Francisco', '352-216-1616')");
+
+            statement.executeUpdate("insert into Barbershop (name, address, phone) values " +
+                    "('Barbershop \"Rusalka\"', '156 Circle Av. Denwer', '261-645-9784')");
+
+            statement.close();
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private void insertOffersInfo() {
+        try {
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht1', 1, '100$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht2', 2, '50$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht3', 3, '40$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht4', 1, '150$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht5', 2, '200$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht6', 3, '130$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht7', 1, '75$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht8', 2, '36$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht1', 3, '40$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht2', 1, '125$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht3', 2, '123$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht4', 3, '256$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht5', 1, '234$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht6', 2, '14$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht7', 3, '63$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht8', 1, '10$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht1', 2, '15$')");
+            statement.executeUpdate("insert into Offer (haircut_id, barbershop_id, price) values ('ht8', 3, '212$')");
+
+            statement.close();
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private void insertCollageInfo() {
+        try {
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate("insert into Collage (id, haircut_id) values ('collage', 'ht5')");
+
+            statement.close();
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
     public String getImagesFolder() {
